b88d444e80d4969205194faba9b5a5b854565f8a
Julio Balbino <julio4110@hotmail.com>
2023-08-18 18:19:16 -0300

    download csv data logs

--- src/main/java/br/dreambricks/heinekenquiz/DataLogController.java
+++ src/main/java/br/dreambricks/heinekenquiz/DataLogController.java
@@ -1,6 +1,8 @@
 package br.dreambricks.heinekenquiz;
 
+import com.opencsv.CSVWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -9,11 +11,17 @@ import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Metrics;
 import org.springframework.data.geo.Point;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequestMapping("/api/datalogs")
@@ -99,5 +107,56 @@ public class DataLogController {
         }else{
             return dataLogRepository.countStatusOccurrences();
         }
+    }
+
+    @GetMapping("/downloaddata")
+    public ResponseEntity<InputStreamResource> downloadData() throws IOException {
+        List<DataLog> dataLogs = dataLogRepository.findAll();
+
+        // Create CSV file
+        File csvFile = new File("data.csv");
+        CSVWriter csvWriter = new CSVWriter(new FileWriter(csvFile));
+        String[] header = {"barName", "uploadedData", "timePlayed", "status", "hits", "miss"};
+        csvWriter.writeNext(header);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        for (DataLog dataLog : dataLogs) {
+            String formattedTimePlayed = dateFormat.format(dataLog.getTimePlayed());
+            String formattedUploadedData = dateFormat.format(dataLog.getUploadedData());
+
+            String[] row = {dataLog.getBarName(),formattedUploadedData, formattedTimePlayed,dataLog.getStatus(), dataLog.getHits(), dataLog.getMiss()};
+            csvWriter.writeNext(row);
+        }
+        csvWriter.close();
+
+        // Create ZIP file
+        File zipFile = new File("data.zip");
+        FileOutputStream fos = new FileOutputStream(zipFile);
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+
+        FileInputStream fis = new FileInputStream(csvFile);
+        ZipEntry zipEntry = new ZipEntry(csvFile.getName());
+        zipOut.putNextEntry(zipEntry);
+
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+        fis.close();
+        zipOut.closeEntry();
+
+        zipOut.close();
+        fos.close();
+
+        // Prepare response entity
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(zipFile));
+
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=data.zip")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(zipFile.length())
+                .body(resource);
     }
 }
